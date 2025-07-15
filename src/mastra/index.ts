8858ce6d27d63b8543b37ac29175e827b0f5447c
Paul Scanlon <pauliescanlon@gmail.com>
2025-07-15 15:22:48 -0500

    test: add @mastra/auth MastraJwtAuth

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,19 +1,24 @@
-
-import { Mastra } from '@mastra/core/mastra';
-import { PinoLogger } from '@mastra/loggers';
-import { LibSQLStore } from '@mastra/libsql';
-import { weatherWorkflow } from './workflows/weather-workflow';
-import { weatherAgent } from './agents/weather-agent';
+import { Mastra } from "@mastra/core/mastra";
+import { MastraJwtAuth } from "@mastra/auth";
+import { PinoLogger } from "@mastra/loggers";
+import { LibSQLStore } from "@mastra/libsql";
+import { weatherWorkflow } from "./workflows/weather-workflow";
+import { weatherAgent } from "./agents/weather-agent";
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
+    url: ":memory:"
   }),
   logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
+    name: "Mastra",
+    level: "info"
   }),
+  server: {
+    experimental_auth: new MastraJwtAuth({
+      secret: "ABC123"
+    })
+  }
 });
